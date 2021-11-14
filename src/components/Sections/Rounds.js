f66e574fa9c60b1990f2c42bb136ef9b51e28a3b
Your Name <tatai.9.f@gmail.com>
2021-11-14 20:11:02 +0530

    add svg animation

--- src/components/Sections/Rounds.js
+++ src/components/Sections/Rounds.js
@@ -1,13 +1,14 @@
-import React, {useState,useEffect} from "react";
+import React, {useState,useEffect,useRef} from "react";
 import styles from "./Rounds.module.css";
 import Roundno from "../Round/Roundno";
 import scrollLock from 'scroll-lock';
+import Rocket from "../Rocket/Rocket";
 import $ from "jquery";
 
 
 function Round (){
     let [scroll,changeScroll]=useState(1);
-
+    let rockRef=useRef();
 
 
 useEffect(()=>{
@@ -32,8 +33,36 @@ useEffect(()=>{
 
     }
 
-
+    function moveRocket(){
+        console.log(scroll)
+        switch(scroll){
+            case 1: 
+           // $("#test").addClass(styles.pos1);
+            rockRef.current.style.transform="translateY(-7vh)";
+            rockRef.current.style.transition="transform 500ms";
+            console.log("hi")
+            break;
+            case 2: 
+            rockRef.current.style.transform="translateY(-13vh)";
+            rockRef.current.style.transition="transform 500ms";
+            break;
+            case 3: 
+            rockRef.current.style.transform="translateY(-20vh)";
+            rockRef.current.style.transition="transform 500ms";
+            break;
+            case 4:
+               // $("#test").removeClass(styles.pos1);
+                rockRef.current.style.transform="translateY(2vh)";
+            rockRef.current.style.transition="transform 500ms";
+            
+            
+        }
+        
+    }
     
+
+
+
 
  
   
@@ -47,7 +76,8 @@ useEffect(()=>{
            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>Nam convallis ornare vestibulum. Integer sed aliquet it</p>
        </div>
 
-     <div className={styles.main} >
+     <div className={styles.main} onWheel={moveRocket}>
+     <div  id={styles.test} ref={rockRef}><Rocket /> </div>
     <ul className={styles.list}>
         <li style={{textShadow:(scroll===4?"1px 1px 10px #fff, 1px 1px 10px #ccc":null)}}>Round 4</li>
         <li style={{textShadow:(scroll===3?"1px 1px 10px #fff, 1px 1px 10px #ccc":null)}}>Round 3</li>
